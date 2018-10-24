3e46c417514bc5132e0d10cd6d8d98ab507fae95
Ian Page Hands <iphands@gmail.com>
2018-10-24 13:37:06 -0400

    push too

--- deploy_everywhere.sh
+++ deploy_everywhere.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git branch -D master
 git checkout master
 git pull
 
@@ -8,6 +7,7 @@ for environment in ci-beta ci-stable qa-beta qa-stable stage-beta stage-stable
 do
     git checkout $environment
     git reset --hard master
+    git push -f origin $environment
 done
 
 git checkout scripts
