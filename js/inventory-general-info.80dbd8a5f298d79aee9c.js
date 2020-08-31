64cb43da475083146efdc659ccbb1530844935f9
Travis CI User <travis@example.org>
2020-08-31 12:30:42 +0000

    Merge pull request #944 from karelhala/update-pf-react-core
    Update PF react core in order to fix global filter select bug

---
+++ js/inventory-general-info.80dbd8a5f298d79aee9c.js
@@ -0,0 +1,2 @@
+(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[13],{733:function(n,o,t){const{customReact:s}=t(168);n.exports=s},734:function(n,o,t){const{PFReact:s}=t(168);n.exports=s}}]);
+//# sourceMappingURL=sourcemaps/inventory-general-info.js.map
