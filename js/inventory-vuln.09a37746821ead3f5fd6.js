af4ceabbfb58d106b7db9efa131f4475cca210d4
Travis CI User <travis@example.org>
2020-09-02 15:33:30 +0000

    Fix inventory bugs by using newer version of inventory package (#945)

---
+++ js/inventory-vuln.09a37746821ead3f5fd6.js
@@ -0,0 +1,2 @@
+(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[17,9,10,12,13,21],{2891:function(n,o,t){const{PFReact:c}=t(271);n.exports=c},2892:function(n,o,t){const{customReact:c}=t(271);n.exports=c},2893:function(n,o,t){const{ReactRouterDOM:c}=t(271);n.exports=c},2920:function(n,o,t){const{PFReactTable:c}=t(271);n.exports=c}}]);
+//# sourceMappingURL=sourcemaps/inventory-vuln.js.map
