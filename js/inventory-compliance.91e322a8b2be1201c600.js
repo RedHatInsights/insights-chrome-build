c83a686e48e52684ab84e0a40859ac000d98b39e
Travis CI User <travis@example.org>
2020-09-14 12:25:02 +0000

    Move things around, make the navigation calculation run in seperate thread (#955)
    * Move things around, make the navigation calculation run in seperate thread
    
    * Simplify navigation and header by introducing loader functions
    
    * Update logic for navigation
    
    * Simplify navigation component
    
    * Refactor how permissions are calculated
    
    * Update loaders of navigation

--- js/inventory-compliance.91e322a8b2be1201c600.js
+++
@@ -1,2 +0,0 @@
-(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[10,9,12,21],{844:function(n,o,t){const{PFReact:c}=t(207);n.exports=c},845:function(n,o,t){const{customReact:c}=t(207);n.exports=c},853:function(n,o,t){const{PFReactTable:c}=t(207);n.exports=c}}]);
-//# sourceMappingURL=sourcemaps/inventory-compliance.js.map
