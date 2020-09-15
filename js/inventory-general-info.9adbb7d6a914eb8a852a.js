7e83bd88c1d953c8c194da81045ddb70920b62b8
Travis CI User <travis@example.org>
2020-09-15 11:13:37 +0000

    ⬆️ Update all npm dependencies (2020-09-15)

---
+++ js/inventory-general-info.9adbb7d6a914eb8a852a.js
@@ -0,0 +1,2 @@
+(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[12],{800:function(n,o,t){const{PFReact:s}=t(131);n.exports=s},801:function(n,o,t){const{customReact:s}=t(131);n.exports=s}}]);
+//# sourceMappingURL=sourcemaps/inventory-general-info.js.map
