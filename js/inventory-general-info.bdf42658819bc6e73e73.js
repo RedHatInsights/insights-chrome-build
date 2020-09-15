c6c7ec61ac5732c130d258217bb1ce6ed2a93263
Travis CI User <travis@example.org>
2020-09-15 10:16:31 +0000

    Introduce chrome cache (#959)
    * Add new chrome global cache
    
    * Write cache tests
    
    * Change search string and storage key for cache

--- js/inventory-general-info.bdf42658819bc6e73e73.js
+++
@@ -1,2 +0,0 @@
-(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[12],{797:function(n,o,t){const{PFReact:s}=t(131);n.exports=s},798:function(n,o,t){const{customReact:s}=t(131);n.exports=s}}]);
-//# sourceMappingURL=sourcemaps/inventory-general-info.js.map
