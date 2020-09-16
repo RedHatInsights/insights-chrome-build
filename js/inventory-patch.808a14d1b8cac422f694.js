7f6fcc0f1913994cf4b4929e84519dd1ee0e0f82
Travis CI User <travis@example.org>
2020-09-16 09:06:00 +0000

    Merge pull request #963 from RedHatInsights/master-stable
    Update prod-stable to newest version

---
+++ js/inventory-patch.808a14d1b8cac422f694.js
@@ -0,0 +1,2 @@
+(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[13],{846:function(n,o,e){const{ReactRouterDOM:s}=e(207);n.exports=s}}]);
+//# sourceMappingURL=sourcemaps/inventory-patch.js.map
