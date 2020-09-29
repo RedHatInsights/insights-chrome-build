29ae390bc5c7cdd62aeb43c17a447fb1dc51eba8
Travis CI User <travis@example.org>
2020-09-29 17:03:53 +0000

    Downgrande pf version. (#994)

---
+++ js/inventory-general-info.2e54434cfd9c4bb09ebc.js
@@ -0,0 +1,2 @@
+(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[11],{992:function(n,o,t){const{PFReact:s}=t(146);n.exports=s},993:function(n,o,t){const{customReact:s}=t(146);n.exports=s}}]);
+//# sourceMappingURL=sourcemaps/inventory-general-info.js.map
--- js/inventory-general-info.855e3031486951bb69f5.js
+++
@@ -1,2 +0,0 @@
-(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[11],{840:function(n,o,t){const{PFReact:s}=t(135);n.exports=s},841:function(n,o,t){const{customReact:s}=t(135);n.exports=s}}]);
-//# sourceMappingURL=sourcemaps/inventory-general-info.js.map
