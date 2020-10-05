a92c660a55b6f897442338d7cbb15ed5de0dc0bc
Travis CI User <travis@example.org>
2020-10-05 15:53:56 +0000

    Merge pull request #997 from karelhala/fix-permissions-gate
    Fix permissions gate by changing it to object

--- js/inventory-patch.808a14d1b8cac422f694.js
+++
@@ -1,2 +0,0 @@
-(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[13],{846:function(n,o,e){const{ReactRouterDOM:s}=e(207);n.exports=s}}]);
-//# sourceMappingURL=sourcemaps/inventory-patch.js.map
