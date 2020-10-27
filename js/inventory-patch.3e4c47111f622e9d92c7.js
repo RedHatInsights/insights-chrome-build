090c8845ff9ed69288fa5c0259453add1b238d71
Travis CI User <travis@example.org>
2020-10-27 15:25:29 +0000

    Enable dynamic navigation items. (#1029)
    * Enable dynamic navigation items.
    
    * Add apiRequest docs.
    
    * Use ids instead of index for extra nav items.
    
    * Remove dynamic nav option.
    
    * Update src/js/consts.js
    
    * Update consts.js
    
    * Update globalNav.test.js
    
    Co-authored-by: Karel Hala <ch4rlien@gmail.com>

--- js/inventory-patch.3e4c47111f622e9d92c7.js
+++
@@ -1,2 +0,0 @@
-(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[14],{964:function(n,o,e){const{ReactRouterDOM:s}=e(235);n.exports=s}}]);
-//# sourceMappingURL=sourcemaps/inventory-patch.js.map
