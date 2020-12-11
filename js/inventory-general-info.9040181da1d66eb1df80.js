79b9d0f0bbabb66691d4d09765763b316c777503
Travis CI User <travis@example.org>
2020-12-11 23:20:31 +0000

    try new travis config (#858)
    * try new travis config
    
    * fix againg
    
    * reformat
    
    * again

--- js/inventory-general-info.9040181da1d66eb1df80.js
+++
@@ -1,2 +0,0 @@
-(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[13],{1278:function(n,o,t){const{PFReact:s}=t(279);n.exports=s},1279:function(n,o,t){const{customReact:s}=t(279);n.exports=s}}]);
-//# sourceMappingURL=sourcemaps/inventory-general-info.js.map
--- js/inventory-patch.389819530fde48daf7d1.js
+++
@@ -1,2 +0,0 @@
-(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[14],{1115:function(n,o,e){const{ReactRouterDOM:s}=e(279);n.exports=s}}]);
-//# sourceMappingURL=sourcemaps/inventory-patch.js.map
