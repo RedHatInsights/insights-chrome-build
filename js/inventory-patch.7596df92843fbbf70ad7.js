784d2493f8c475bf9bc19332e80837fa6b161cfc
Travis CI User <travis@example.org>
2020-12-11 15:49:14 +0000

    Update version of FEC to newest version on 20-12-11 (#1100)

---
+++ js/inventory-patch.7596df92843fbbf70ad7.js
@@ -0,0 +1,2 @@
+(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[12],{991:function(n,o,e){const{ReactRouterDOM:s}=e(233);n.exports=s}}]);
+//# sourceMappingURL=sourcemaps/inventory-patch.js.map
