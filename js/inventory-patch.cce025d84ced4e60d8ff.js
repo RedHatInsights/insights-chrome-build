bf7b20469afb9bd0e1f774514602321af755ef96
Travis CI User <travis@example.org>
2021-01-05 08:21:34 +0000

    Merge pull request #1101 from PanSpagetka/add-app-filter
    Add app filter

---
+++ js/inventory-patch.cce025d84ced4e60d8ff.js
@@ -0,0 +1,2 @@
+(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[12],{993:function(n,o,e){const{ReactRouterDOM:s}=e(234);n.exports=s}}]);
+//# sourceMappingURL=sourcemaps/inventory-patch.js.map
