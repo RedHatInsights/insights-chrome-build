1ef90ac718015ffb2770eb6da60249b5179051f6
Travis CI User <travis@example.org>
2021-01-25 12:29:25 +0000

    remove all the temp nav styling (#1136)
    Co-authored-by: Karel Hala <khala@redhat.com>

--- js/inventory.5d76a50246e23349ac2b.js
+++
@@ -1,2 +0,0 @@
-(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[10,12],{1113:function(t,n,o){const{ReactRouterDOM:c}=o(239);t.exports=c},1751:function(t,n,o){const{PFReact:c}=o(239);t.exports=c},1752:function(t,n,o){const{customReact:c}=o(239);t.exports=c},1753:function(t,n,o){const{reactRedux:c}=o(239);t.exports=c},1754:function(t,n,o){const{PFReactTable:c}=o(239);t.exports=c}}]);
-//# sourceMappingURL=sourcemaps/inventory.js.map
