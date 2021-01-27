e9130dfe6b7797010f9bcf6c71cb9ff41101e002
Travis CI User <travis@example.org>
2021-01-27 14:15:24 +0000

    Use full width for global filter (#1137)

---
+++ js/inventory-patch.f17402fd88100d12b53a.js
@@ -0,0 +1,2 @@
+(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[12],{1115:function(n,o,e){const{ReactRouterDOM:s}=e(241);n.exports=s}}]);
+//# sourceMappingURL=sourcemaps/inventory-patch.js.map
