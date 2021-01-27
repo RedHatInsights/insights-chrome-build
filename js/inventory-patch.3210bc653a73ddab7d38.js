e1e96226e1225724b9819865aaa81b9f14d490a4
Travis CI User <travis@example.org>
2021-01-27 23:45:34 +0000

    try new travis config (#858)
    * try new travis config
    
    * fix againg
    
    * reformat
    
    * again

---
+++ js/inventory-patch.3210bc653a73ddab7d38.js
@@ -0,0 +1,2 @@
+(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[12],{1276:function(n,o,e){const{ReactRouterDOM:s}=e(286);n.exports=s}}]);
+//# sourceMappingURL=sourcemaps/inventory-patch.js.map
