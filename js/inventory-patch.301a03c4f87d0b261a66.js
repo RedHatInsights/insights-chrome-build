f5761b717651dab1aea8cc3d1bdcdd7c31f85ee3
Travis CI User <travis@example.org>
2021-02-04 09:33:38 +0000

    Merge pull request #1153 from karelhala/sid-filter
    Add SIDs filter to both global filter and tags modal

---
+++ js/inventory-patch.301a03c4f87d0b261a66.js
@@ -0,0 +1,2 @@
+(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[12],{1097:function(n,o,e){const{ReactRouterDOM:s}=e(242);n.exports=s}}]);
+//# sourceMappingURL=sourcemaps/inventory-patch.js.map
