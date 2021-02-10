012d35754d8eb6c0362b9765dc376fed5c81bdda
Travis CI User <travis@example.org>
2021-02-10 10:10:41 +0000

    Update remediations package on 21-02-10 (#1175)

--- js/inventory-patch.301a03c4f87d0b261a66.js
+++
@@ -1,2 +0,0 @@
-(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[12],{1097:function(n,o,e){const{ReactRouterDOM:s}=e(242);n.exports=s}}]);
-//# sourceMappingURL=sourcemaps/inventory-patch.js.map
