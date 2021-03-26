b7bed83554a632640f9cdd53abf4a9b97928c60e
Travis CI User <travis@example.org>
2021-03-26 12:31:40 +0000

    Update frontend component version (#1292)

--- js/inventory.57c5498984ac567dd8f0.js
+++
@@ -1,2 +0,0 @@
-(window.wpJsonpChromeInstance=window.wpJsonpChromeInstance||[]).push([[9,11],{1102:function(t,n,o){const{ReactRouterDOM:c}=o(242);t.exports=c},1731:function(t,n,o){const{PFReact:c}=o(242);t.exports=c},1732:function(t,n,o){const{customReact:c}=o(242);t.exports=c},1733:function(t,n,o){const{reactRedux:c}=o(242);t.exports=c},1734:function(t,n,o){const{PFReactTable:c}=o(242);t.exports=c}}]);
-//# sourceMappingURL=sourcemaps/inventory.js.map
