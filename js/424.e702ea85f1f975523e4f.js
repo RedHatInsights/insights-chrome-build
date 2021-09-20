ffed062fcc5d3ef1a3bf6c814421d634fbf2743b
Travis CI User <travis@example.org>
2021-09-20 10:20:27 +0000

    Merge pull request #1646 from karelhala/add-pdf-proxy
    Add pdf download button as proxy, so we can use the button locally

---
+++ js/424.e702ea85f1f975523e4f.js
@@ -0,0 +1,1 @@
+(self.webpackChunkinsights_chrome=self.webpackChunkinsights_chrome||[]).push([[424],{82118:(e,n,s)=>{"use strict";s.r(n),s.d(n,{default:()=>h});var t=s(96985),c=s.n(t),r=s(58141);const h=function(e){return c().createElement(r.o6,e)}},25832:()=>{},69862:()=>{},40964:()=>{}}]);
