dc4cd1b9245036d1e13a17017de913279a312cfb
Travis CI User <travis@example.org>
2021-09-21 10:12:08 +0000

    Merge pull request #1648 from karelhala/update-pdf-generator
    Use latest version of pdf generator

--- js/424.78ab8d8829311f5d966c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkinsights_chrome=self.webpackChunkinsights_chrome||[]).push([[424],{82118:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>u});var r=t(96985),s=t.n(r),c=t(58141);const u=function(e){return s().createElement(c.DownloadButtonWrapper,e)}},25832:()=>{},69862:()=>{},40964:()=>{}}]);
