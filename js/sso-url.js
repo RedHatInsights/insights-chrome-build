091bcb90781ae406fc04dbed607914dab5b5d798
Travis CI User <travis@example.org>
2022-07-07 13:14:08 +0000

    Merge pull request #1953 from RedHatInsights/master
    [stable] Update build

---
+++ js/sso-url.js
@@ -0,0 +1,18 @@
+"use strict";
+(self["webpackChunkinsights_chrome"] = self["webpackChunkinsights_chrome"] || []).push([["sso-url"],{
+
+/***/ "./src/js/jwt/insights/ssoUrl.js":
+/*!***************************************!*\
+  !*** ./src/js/jwt/insights/ssoUrl.js ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ('');
+
+/***/ })
+
+}]);
