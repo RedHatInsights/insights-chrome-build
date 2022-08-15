89b15f83d8950d41a5460e7a28a53f325561045a
Travis CI User <travis@example.org>
2022-08-15 10:03:45 +0000

    Merge pull request #2006 from RedHatInsights/master
    [stable] Update build

--- js/chrome.dae0523b22d7b05cb7eb.js
+++ js/chrome.dae0523b22d7b05cb7eb.js
@@ -32455,7 +32455,7 @@ __webpack_require__.d(exports, {
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("d6aa839a154d6e4b4557")
+/******/ 		__webpack_require__.h = () => ("dae0523b22d7b05cb7eb")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
