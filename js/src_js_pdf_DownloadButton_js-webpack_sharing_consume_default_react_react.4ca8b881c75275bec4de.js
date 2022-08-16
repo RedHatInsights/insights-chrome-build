b5220749ab7fb639474fcae808ba7630fb73bfd8
Travis CI User <travis@example.org>
2022-08-16 13:10:10 +0000

    Merge pull request #2007 from Hyperkid123/visual-regression-testing
    Visual regression testing

---
+++ js/src_js_pdf_DownloadButton_js-webpack_sharing_consume_default_react_react.4ca8b881c75275bec4de.js
@@ -0,0 +1,99 @@
+(self["webpackChunkinsights_chrome"] = self["webpackChunkinsights_chrome"] || []).push([["src_js_pdf_DownloadButton_js-webpack_sharing_consume_default_react_react"],{
+
+/***/ "./src/js/pdf/DownloadButton.js":
+/*!**************************************!*\
+  !*** ./src/js/pdf/DownloadButton.js ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "webpack/sharing/consume/default/react/react?474c");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _redhat_cloud_services_frontend_components_pdf_generator__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @redhat-cloud-services/frontend-components-pdf-generator */ "./node_modules/@redhat-cloud-services/frontend-components-pdf-generator/dist/esm/index.js");
+/* eslint-disable rulesdir/disallow-fec-relative-imports */
+
+
+
+var ProxyDownloadButton = function ProxyDownloadButton(props) {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_redhat_cloud_services_frontend_components_pdf_generator__WEBPACK_IMPORTED_MODULE_1__.DownloadButtonWrapper, props);
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ProxyDownloadButton);
+
+/***/ }),
+
+/***/ "?e6c7":
+/*!***************************************************************************!*\
+  !*** @patternfly/react-styles/css/components/Popper/Popper.css (ignored) ***!
+  \***************************************************************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?650f":
+/*!*******************************!*\
+  !*** ./spinner.css (ignored) ***!
+  \*******************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?afff":
+/*!*******************************!*\
+  !*** ./tooltip.css (ignored) ***!
+  \*******************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?2fb3":
+/*!********************************!*\
+  !*** ./bullseye.css (ignored) ***!
+  \********************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?943a":
+/*!************************!*\
+  !*** stream (ignored) ***!
+  \************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?3e83":
+/*!**********************!*\
+  !*** util (ignored) ***!
+  \**********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?19e6":
+/*!**********************!*\
+  !*** util (ignored) ***!
+  \**********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
