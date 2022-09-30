e2ecfec575e240f55e674b7b449783bf09044101
Travis CI User <travis@example.org>
2022-09-30 11:36:47 +0000

    Banned accounts handler (#2061)
    * Update scalprum dependency.
    
    * Add gateway response interceptor function.
    
    * Move default error component.
    
    * Create gateway error component.
    
    * Hook gateway interceptor into global fetch and XHR definition.
    
    * Add e2e test for gateway errors.

---
+++ js/quick-start.ecc9860c0ef832e80bb9.js
@@ -0,0 +1,33 @@
+"use strict";
+(self["webpackChunkinsights_chrome"] = self["webpackChunkinsights_chrome"] || []).push([["quick-start"],{
+
+/***/ "./src/js/App/QuickStart/QuickStartCatalog.js":
+/*!****************************************************!*\
+  !*** ./src/js/App/QuickStart/QuickStartCatalog.js ***!
+  \****************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _babel_runtime_helpers_extends__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/extends */ "./node_modules/@babel/runtime/helpers/esm/extends.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "webpack/sharing/consume/default/react/react?474c");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _patternfly_quickstarts__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @patternfly/quickstarts */ "webpack/sharing/consume/default/@patternfly/quickstarts/@patternfly/quickstarts");
+/* harmony import */ var _patternfly_quickstarts__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_patternfly_quickstarts__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+
+var QuickStartCatalog = function QuickStartCatalog(_ref) {
+  var props = (0,_babel_runtime_helpers_extends__WEBPACK_IMPORTED_MODULE_0__.default)({}, _ref);
+
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement(_patternfly_quickstarts__WEBPACK_IMPORTED_MODULE_2__.QuickStartCatalogPage, props);
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (QuickStartCatalog);
+
+/***/ })
+
+}]);
