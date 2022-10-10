4083369fbbf4c48735da5fef5712e21a187687a8
Travis CI User <travis@example.org>
2022-10-10 07:51:34 +0000

    Merge pull request #2055 from zwitter1/CIAM-AddSettingsOptions
    Ciam add settings options

---
+++ js/2736.e814d2c79eae366cb969.js
@@ -0,0 +1,1 @@
+(self.webpackChunkinsights_chrome=self.webpackChunkinsights_chrome||[]).push([[2736],{17061:(e,n,s)=>{"use strict";s.r(n),s.d(n,{default:()=>h});var t=s(96985),r=s.n(t),c=s(58141);const h=function(e){return r().createElement(c.rU,e)}},45467:()=>{},66822:()=>{},49854:()=>{},87234:()=>{},25832:()=>{},69862:()=>{},40964:()=>{}}]);
