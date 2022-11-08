11c95266df7f7760805c79ccb4a7117096036691
Travis CI User <travis@example.org>
2022-11-08 08:33:00 +0000

    Merge pull request #2082 from aneelac22/feature/RHCLOUD-20863
    Add Report Bug and Inform Direction

--- js/chrome-root.877142080bbd7ed3884f.js
+++ js/chrome-root.877142080bbd7ed3884f.js
@@ -32472,7 +32472,7 @@ Promise.all(/*! import() */[__webpack_require__.e("vendors-node_modules_prop-typ
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("842b2c7e2e4366d4253f")
+/******/ 		__webpack_require__.h = () => ("877142080bbd7ed3884f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
