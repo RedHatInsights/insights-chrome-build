24cc7db7eb1bd5710369c512f6b985e89915ef88
Travis CI User <travis@example.org>
2022-11-11 12:35:18 +0000

    Merge pull request #2113 from gkarat/fix-window-fetch-issue
    fix: Clone response before accessing the body

--- js/chrome-root.215b6f16f4d6e39a.js
+++ js/chrome-root.215b6f16f4d6e39a.js
@@ -32472,7 +32472,7 @@ Promise.all(/*! import() */[__webpack_require__.e("vendors-node_modules_prop-typ
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("69ffada970f31941")
+/******/ 		__webpack_require__.h = () => ("215b6f16f4d6e39a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
