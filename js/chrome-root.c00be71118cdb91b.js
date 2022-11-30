2e95cc84cc203f6baa9eca3e591957f715c070e9
Travis CI User <travis@example.org>
2022-11-30 08:36:18 +0000

    Merge pull request #2127 from gkarat/patch-3
    Decrease sample rate to meet transaction quotas

--- js/chrome-root.c00be71118cdb91b.js
+++ js/chrome-root.c00be71118cdb91b.js
@@ -32472,7 +32472,7 @@ Promise.all(/*! import() */[__webpack_require__.e("vendors-node_modules_prop-typ
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f009cd950bb2f015")
+/******/ 		__webpack_require__.h = () => ("c00be71118cdb91b")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
