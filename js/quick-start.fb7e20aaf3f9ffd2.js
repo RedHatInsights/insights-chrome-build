adff5d4306c1c67589f09a41f1e1bb8cd9888bca
Travis CI User <travis@example.org>
2023-01-02 13:56:13 +0000

    Merge pull request #2176 from Hyperkid123/dependency-clean-up
    Dependency clean up

---
+++ js/quick-start.fb7e20aaf3f9ffd2.js
@@ -0,0 +1,26 @@
+"use strict";
+(self["webpackChunkinsights_chrome"] = self["webpackChunkinsights_chrome"] || []).push([["quick-start"],{
+
+/***/ "./src/components/QuickStart/QuickStartCatalog.tsx":
+/*!*********************************************************!*\
+  !*** ./src/components/QuickStart/QuickStartCatalog.tsx ***!
+  \*********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "webpack/sharing/consume/default/react/react?88de");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _patternfly_quickstarts__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @patternfly/quickstarts */ "webpack/sharing/consume/default/@patternfly/quickstarts/@patternfly/quickstarts");
+/* harmony import */ var _patternfly_quickstarts__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_patternfly_quickstarts__WEBPACK_IMPORTED_MODULE_1__);
+
+
+const QuickStartCatalog = ({ ...props }) => react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_patternfly_quickstarts__WEBPACK_IMPORTED_MODULE_1__.QuickStartCatalogPage, { ...props });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (QuickStartCatalog);
+
+
+/***/ })
+
+}]);
