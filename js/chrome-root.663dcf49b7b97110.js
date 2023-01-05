ee3a2e7afbe92d206acc469831fa321e40d6b8f6
Travis CI User <travis@example.org>
2023-01-05 13:23:42 +0000

    Merge pull request #2178 from Hyperkid123/add-feature-flags-fallback
    Silently fail if feature flags API throws server error.

--- js/chrome-root.663dcf49b7b97110.js
+++ js/chrome-root.663dcf49b7b97110.js
@@ -32472,7 +32472,7 @@ Promise.all(/*! import() */[__webpack_require__.e("vendors-node_modules_prop-typ
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("6bfc63faf9ce44b9")
+/******/ 		__webpack_require__.h = () => ("663dcf49b7b97110")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
