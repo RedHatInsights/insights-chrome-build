53420a673e1fd1557a355fe46ebb0185ac789fbd
Travis CI User <travis@example.org>
2023-01-20 10:19:47 +0000

    Merge pull request #2239 from Hyperkid123/fix-last-visited-titles
    Fix last visited titles

--- js/chrome.0bc9bd73404b41d4.js
+++ js/chrome.0bc9bd73404b41d4.js
@@ -32451,7 +32451,7 @@ __webpack_require__.d(exports, {
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("301e9cc626f9a21a")
+/******/ 		__webpack_require__.h = () => ("0bc9bd73404b41d4")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
