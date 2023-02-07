ffd63b5e4d31d924db85be97b11fc5565aa90146
Travis CI User <travis@example.org>
2023-02-07 12:39:01 +0000

    Do not send auth header outside of origin (#2267)

--- js/chrome-root.e49f63b97b2a2104.js
+++ js/chrome-root.e49f63b97b2a2104.js
@@ -32472,7 +32472,7 @@ Promise.all(/*! import() */[__webpack_require__.e("vendors-node_modules_prop-typ
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e1e9d38ef845aea3")
+/******/ 		__webpack_require__.h = () => ("e49f63b97b2a2104")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
