d6f5115e1149e2bd24280065ebef3307baf03fc2
Travis CI User <travis@example.org>
2023-02-13 12:51:15 +0000

    Merge pull request #2258 from adonispuente/sentry
    Add sentry replay to config

--- js/chrome.30585ac7fac76baa.js
+++ js/chrome.30585ac7fac76baa.js
@@ -32451,7 +32451,7 @@ __webpack_require__.d(exports, {
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("6cf35db0f0070471")
+/******/ 		__webpack_require__.h = () => ("30585ac7fac76baa")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
