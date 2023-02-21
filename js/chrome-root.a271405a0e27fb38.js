960436e971c2e95eac117d8f2636459a7310f0d1
Travis CI User <travis@example.org>
2023-02-21 14:51:56 +0000

    Merge pull request #2299 from karelhala/allow-dev-foo
    Add dev.foo.redhat.com to list of available prod sso

--- js/chrome-root.a271405a0e27fb38.js
+++ js/chrome-root.a271405a0e27fb38.js
@@ -32472,7 +32472,7 @@ Promise.all(/*! import() */[__webpack_require__.e("vendors-node_modules_prop-typ
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("7b0727d96c096044")
+/******/ 		__webpack_require__.h = () => ("a271405a0e27fb38")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
