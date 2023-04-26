73376e246e3e61599ff5eae9dfaf0f088d62b2e2
Travis CI User <travis@example.org>
2023-04-26 07:35:50 +0000

    Merge pull request #2459 from epwinchell/fix_fav
    Fix Favoriting hover behavior

--- js/chrome.113252aff7f96582.js
+++ js/chrome.113252aff7f96582.js
@@ -32451,7 +32451,7 @@ __webpack_require__.d(exports, {
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f397b7cfc2f3d2dd")
+/******/ 		__webpack_require__.h = () => ("113252aff7f96582")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
