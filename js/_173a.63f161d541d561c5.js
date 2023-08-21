568d0b5dda7fc4888bd8590ea454319d41237dee
Travis CI User <travis@example.org>
2023-08-21 13:46:27 +0000

    Merge pull request #2551 from Hyperkid123/pf5-testing
    PF5 release

---
+++ js/_173a.63f161d541d561c5.js
@@ -0,0 +1,13 @@
+(self["webpackChunkinsights_chrome"] = self["webpackChunkinsights_chrome"] || []).push([["_173a"],{
+
+/***/ "?173a":
+/*!********************************!*\
+  !*** ./dropdown.css (ignored) ***!
+  \********************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
---
+++ js/_9b31.8dfbb3484ac74f3e.js
@@ -0,0 +1,13 @@
+(self["webpackChunkinsights_chrome"] = self["webpackChunkinsights_chrome"] || []).push([["_9b31"],{
+
+/***/ "?9b31":
+/*!**************************************!*\
+  !*** ./clipboard-copy.css (ignored) ***!
+  \**************************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
