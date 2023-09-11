4401c09020d29738dbefb035a96bf67edd663d27
Travis CI User <travis@example.org>
2023-09-11 10:14:52 +0000

    Merge pull request #2618 from RedHatInsights/stable-merge
    [stable] Update build

---
+++ js/src_pdf_DownloadButton_tsx-webpack_sharing_consume_default_react_react.afdc470990922d08.js
@@ -0,0 +1,87 @@
+(self["webpackChunkinsights_chrome"] = self["webpackChunkinsights_chrome"] || []).push([["src_pdf_DownloadButton_tsx-webpack_sharing_consume_default_react_react"],{
+
+/***/ "./src/pdf/DownloadButton.tsx":
+/*!************************************!*\
+  !*** ./src/pdf/DownloadButton.tsx ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "webpack/sharing/consume/default/react/react?dc4e");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _redhat_cloud_services_frontend_components_pdf_generator__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @redhat-cloud-services/frontend-components-pdf-generator */ "./node_modules/@redhat-cloud-services/frontend-components-pdf-generator/dist/esm/index.js");
+/* eslint-disable rulesdir/disallow-fec-relative-imports */ 
+
+var ProxyDownloadButton = function(props) {
+    return /*#__PURE__*/ react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_redhat_cloud_services_frontend_components_pdf_generator__WEBPACK_IMPORTED_MODULE_1__.DownloadButtonWrapper, props);
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ProxyDownloadButton);
+
+
+/***/ }),
+
+/***/ "?1a3c":
+/*!*****************************!*\
+  !*** ./badge.css (ignored) ***!
+  \*****************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?0f6c":
+/*!******************************!*\
+  !*** ./button.css (ignored) ***!
+  \******************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?4a7c":
+/*!*******************************!*\
+  !*** ./spinner.css (ignored) ***!
+  \*******************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?04f2":
+/*!************************!*\
+  !*** stream (ignored) ***!
+  \************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?c852":
+/*!**********************!*\
+  !*** util (ignored) ***!
+  \**********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?8c28":
+/*!**********************!*\
+  !*** util (ignored) ***!
+  \**********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
