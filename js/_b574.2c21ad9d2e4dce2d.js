0a30db9c3701d16a3dc809cb742b040a12c18d14
Travis CI User <travis@example.org>
2023-10-10 18:01:46 +0000

    Merge pull request #2659 from RedHatInsights/master
    [stable] Update build

---
+++ js/_b574.2c21ad9d2e4dce2d.js
@@ -0,0 +1,13 @@
+(self["webpackChunkinsights_chrome"] = self["webpackChunkinsights_chrome"] || []).push([["_b574"],{
+
+/***/ "?b574":
+/*!**********************************!*\
+  !*** ./pagination.css (ignored) ***!
+  \**********************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
