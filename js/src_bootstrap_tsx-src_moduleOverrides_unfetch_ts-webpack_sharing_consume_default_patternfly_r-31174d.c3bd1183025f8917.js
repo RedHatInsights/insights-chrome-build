405016cc8ba9343c3619cc9c765708e86d0c0be9
Travis CI User <travis@example.org>
2023-11-16 13:40:28 +0000

    Merge pull request #2701 from Hyperkid123/disable-pendo-landing-page
    Disable pendo on stage landing page.

--- js/src_bootstrap_tsx-src_moduleOverrides_unfetch_ts-webpack_sharing_consume_default_patternfly_r-31174d.c3bd1183025f8917.js
+++ js/src_bootstrap_tsx-src_moduleOverrides_unfetch_ts-webpack_sharing_consume_default_patternfly_r-31174d.c3bd1183025f8917.js
@@ -14868,6 +14868,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _hooks_useChromeServiceEvents__WEBPACK_IMPORTED_MODULE_27__ = __webpack_require__(/*! ../../hooks/useChromeServiceEvents */ "./src/hooks/useChromeServiceEvents.ts");
 /* harmony import */ var _redux_actions__WEBPACK_IMPORTED_MODULE_28__ = __webpack_require__(/*! ../../redux/actions */ "./src/redux/actions.ts");
 /* harmony import */ var _hooks_useTrackPendoUsage__WEBPACK_IMPORTED_MODULE_29__ = __webpack_require__(/*! ../../hooks/useTrackPendoUsage */ "./src/hooks/useTrackPendoUsage.ts");
+/* harmony import */ var _hooks_useDisablePendoOnLanding__WEBPACK_IMPORTED_MODULE_30__ = __webpack_require__(/*! ../../hooks/useDisablePendoOnLanding */ "./src/hooks/useDisablePendoOnLanding.ts");
 function _array_like_to_array(arr, len) {
     if (len == null || len > arr.length) len = arr.length;
     for(var i = 0, arr2 = new Array(len); i < len; i++)arr2[i] = arr[i];
@@ -15126,6 +15127,7 @@ function _ts_generator(thisArg, body) {
 
 
 
+
 var ProductSelection = /*#__PURE__*/ (0,react__WEBPACK_IMPORTED_MODULE_0__.lazy)(function() {
     return __webpack_require__.e(/*! import() */ "src_components_Stratosphere_ProductSelection_tsx").then(__webpack_require__.bind(__webpack_require__, /*! ../Stratosphere/ProductSelection */ "./src/components/Stratosphere/ProductSelection.tsx"));
 });
@@ -15173,6 +15175,8 @@ var ScalprumRoot = /*#__PURE__*/ (0,react__WEBPACK_IMPORTED_MODULE_0__.memo)(fun
     (0,_hooks_useChromeServiceEvents__WEBPACK_IMPORTED_MODULE_27__["default"])();
     // track pendo usage
     (0,_hooks_useTrackPendoUsage__WEBPACK_IMPORTED_MODULE_29__["default"])();
+    // disable guides on landing page
+    (0,_hooks_useDisablePendoOnLanding__WEBPACK_IMPORTED_MODULE_30__["default"])();
     function getNotifications() {
         return _getNotifications.apply(this, arguments);
     }
@@ -17357,6 +17361,74 @@ var useChromeServiceEvents = function() {
     ]);
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (useChromeServiceEvents);
+
+
+/***/ }),
+
+/***/ "./src/hooks/useDisablePendoOnLanding.ts":
+/*!***********************************************!*\
+  !*** ./src/hooks/useDisablePendoOnLanding.ts ***!
+  \***********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "webpack/sharing/consume/default/react/react?dc4e");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "webpack/sharing/consume/default/react-redux/react-redux");
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_redux__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _utils_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../utils/common */ "./src/utils/common.ts");
+/* harmony import */ var _utils_consts__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../utils/consts */ "./src/utils/consts.ts");
+
+
+
+
+// interval timing is short because we want to catch the bubble before ASAP so it does not cover the VA button
+var RETRY_ATTEMPS = 500;
+var RETRY_INTERVAL = 50;
+var useDisablePendoOnLanding = function() {
+    var activeModule = (0,react_redux__WEBPACK_IMPORTED_MODULE_1__.useSelector)(function(state) {
+        return state.chrome.activeModule;
+    });
+    var toggleGuides = function() {
+        if (window.pendo && activeModule === "landing") {
+            window.pendo.stopGuides();
+        } else {
+            var _window_pendo;
+            (_window_pendo = window.pendo) === null || _window_pendo === void 0 ? void 0 : _window_pendo.startGuides();
+        }
+    };
+    (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(function() {
+        var attempt = 0;
+        var interval = undefined;
+        if (window.pendo) {
+            toggleGuides();
+        } else {
+            interval = setInterval(function() {
+                if (attempt < RETRY_ATTEMPS) {
+                    attempt += 1;
+                    if (window.pendo) {
+                        clearInterval(interval);
+                        toggleGuides();
+                    }
+                } else {
+                    clearInterval(interval);
+                }
+            }, RETRY_INTERVAL);
+        }
+        return function() {
+            if (interval && !(0,_utils_common__WEBPACK_IMPORTED_MODULE_2__.isProd)() && !_utils_consts__WEBPACK_IMPORTED_MODULE_3__.isITLessEnv) {
+                clearInterval(interval);
+            }
+        };
+    }, [
+        activeModule
+    ]);
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (useDisablePendoOnLanding);
 
 
 /***/ }),
