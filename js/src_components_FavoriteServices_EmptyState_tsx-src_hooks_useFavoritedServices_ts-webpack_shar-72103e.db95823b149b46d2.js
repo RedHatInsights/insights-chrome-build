166b5e9952c429f9659450a797fede01a3ca32ef
Travis CI User <travis@example.org>
2023-12-11 09:00:15 +0000

    Merge pull request #2718 from aneelac22/add-refresh
    Add getRefreshToken for ITLess env

--- js/src_components_FavoriteServices_EmptyState_tsx-src_hooks_useFavoritedServices_ts-webpack_shar-72103e.db95823b149b46d2.js
+++ js/src_components_FavoriteServices_EmptyState_tsx-src_hooks_useFavoritedServices_ts-webpack_shar-72103e.db95823b149b46d2.js
@@ -84,6 +84,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 
 var blankUser = {
+    scope: [],
     entitlements: {},
     identity: {
         org_id: "",
@@ -105,6 +106,9 @@ var ChromeAuthContext = (0,react__WEBPACK_IMPORTED_MODULE_0__.createContext)({
         return Promise.resolve();
     },
     getToken: function() {
+        return Promise.resolve("");
+    },
+    getRefreshToken: function() {
         return Promise.resolve("");
     },
     getOfflineToken: function() {
