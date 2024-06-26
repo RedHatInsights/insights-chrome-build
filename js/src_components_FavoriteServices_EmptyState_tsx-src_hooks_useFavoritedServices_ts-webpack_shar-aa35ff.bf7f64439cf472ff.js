83dcea310f12ddb4cd005fee0fa0b78772c0cc1e
Travis CI User <travis@example.org>
2024-06-26 13:58:01 +0000

    Merge pull request #2873 from Hyperkid123/feature-flags-init
    Duplicate FF context before updating preview env.

--- js/src_components_FavoriteServices_EmptyState_tsx-src_hooks_useFavoritedServices_ts-webpack_shar-aa35ff.bf7f64439cf472ff.js
+++ js/src_components_FavoriteServices_EmptyState_tsx-src_hooks_useFavoritedServices_ts-webpack_shar-aa35ff.bf7f64439cf472ff.js
@@ -4323,6 +4323,58 @@ function _async_to_generator(fn) {
         });
     };
 }
+function _define_property(obj, key, value) {
+    if (key in obj) {
+        Object.defineProperty(obj, key, {
+            value: value,
+            enumerable: true,
+            configurable: true,
+            writable: true
+        });
+    } else {
+        obj[key] = value;
+    }
+    return obj;
+}
+function _object_spread(target) {
+    for(var i = 1; i < arguments.length; i++){
+        var source = arguments[i] != null ? arguments[i] : {};
+        var ownKeys = Object.keys(source);
+        if (typeof Object.getOwnPropertySymbols === "function") {
+            ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function(sym) {
+                return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+            }));
+        }
+        ownKeys.forEach(function(key) {
+            _define_property(target, key, source[key]);
+        });
+    }
+    return target;
+}
+function ownKeys(object, enumerableOnly) {
+    var keys = Object.keys(object);
+    if (Object.getOwnPropertySymbols) {
+        var symbols = Object.getOwnPropertySymbols(object);
+        if (enumerableOnly) {
+            symbols = symbols.filter(function(sym) {
+                return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+            });
+        }
+        keys.push.apply(keys, symbols);
+    }
+    return keys;
+}
+function _object_spread_props(target, source) {
+    source = source != null ? source : {};
+    if (Object.getOwnPropertyDescriptors) {
+        Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+    } else {
+        ownKeys(Object(source)).forEach(function(key) {
+            Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+        });
+    }
+    return target;
+}
 function _ts_generator(thisArg, body) {
     var f, y, t, g, _ = {
         label: 0,
@@ -4451,11 +4503,11 @@ var isPreviewAtom = (0,_utils__WEBPACK_IMPORTED_MODULE_2__.atomWithToggle)(undef
                 case 2:
                     if ((0,_components_FeatureFlags_unleashClient__WEBPACK_IMPORTED_MODULE_3__.unleashClientExists)()) {
                         // Required to change the `platform.chrome.ui.preview` context in the feature flags, TS is bugged
-                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                        // @ts-ignore
-                        (0,_components_FeatureFlags_unleashClient__WEBPACK_IMPORTED_MODULE_3__.getUnleashClient)().updateContext({
+                        (0,_components_FeatureFlags_unleashClient__WEBPACK_IMPORTED_MODULE_3__.getUnleashClient)().updateContext(_object_spread_props(_object_spread({}, (0,_components_FeatureFlags_unleashClient__WEBPACK_IMPORTED_MODULE_3__.getUnleashClient)().getContext()), {
+                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                            // @ts-ignore
                             "platform.chrome.ui.preview": isPreview
-                        });
+                        }));
                     }
                     return [
                         3,
