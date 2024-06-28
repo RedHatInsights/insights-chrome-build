18f423fcebb9088271979e2fd985ccda313fc808
Travis CI User <travis@example.org>
2024-06-28 11:29:49 +0000

    Merge pull request #2876 from Hyperkid123/update-pf
    Update minor dependecy versions.

---
+++ js/_1244.46d73e74776a1584.js
@@ -0,0 +1,13 @@
+(self["webpackChunkinsights_chrome"] = self["webpackChunkinsights_chrome"] || []).push([["_1244"],{
+
+/***/ "?1244":
+/*!*******************************************!*\
+  !*** ./notification-drawer.css (ignored) ***!
+  \*******************************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
